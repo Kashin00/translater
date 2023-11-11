3ba7157c9595e57a670dbeae05a34499ef2f4836
Matthew <kashin.developer@gmail.com>
2023-11-11 18:44:30 +0200

    codable scene configuration

--- Translator/Translator/MainViewController.swift
+++ Translator/Translator/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Translator
 //
 //  Created by Матвей Кашин on 11.11.2023.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
