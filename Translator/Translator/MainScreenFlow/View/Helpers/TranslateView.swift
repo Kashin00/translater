991b63af262c7bfa73f37d4862e3b8af617627fa
Matthew <kashin.developer@gmail.com>
2023-11-11 21:14:57 +0200

    add devider

--- Translator/Translator/MainScreenFlow/View/Helpers/TranslateView.swift
+++ Translator/Translator/MainScreenFlow/View/Helpers/TranslateView.swift
@@ -16,10 +16,20 @@ final class TranslateView: UIView {
         return $0
     }(EntryTextView(frame: .zero))
     
+    private lazy var deviderView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .systemBlue
+        $0.layer.masksToBounds = true
+        $0.layer.cornerRadius = 2
+        return $0
+    }(UIView(frame: .zero))
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .gray
         setupEntryTextView()
+        setupDeviderView()
     }
     
     required init?(coder: NSCoder) {
@@ -43,5 +53,16 @@ private extension TranslateView {
         entryTextView.typedTextHandler = { [weak self] in
             self?.typedTextHandler?($0)
         }
+    }
+    
+    func setupDeviderView() {
+        addSubview(deviderView)
+        
+        NSLayoutConstraint.activate([
+            deviderView.topAnchor.constraint(equalTo: entryTextView.bottomAnchor),
+            deviderView.heightAnchor.constraint(equalToConstant: 4),
+            deviderView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            deviderView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5)
+        ])
     }
 }
