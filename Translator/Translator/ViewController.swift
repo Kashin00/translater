05c296ff959e11cc56b29a5eaf6477ad41ee9ea6
Matthew <kashin.developer@gmail.com>
2023-11-11 18:18:37 +0200

    add empty project

---
+++ Translator/Translator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Translator
+//
+//  Created by Матвей Кашин on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
